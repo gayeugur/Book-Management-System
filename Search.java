7b36a4d13586b16877ee80c0636295f626325d71
gayeugur <78750442+gayeugur@users.noreply.github.com>
2021-02-17 00:40:10 +0300

    Add files via upload

---
+++ Search.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bp;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author gayeu
+ */
+public interface Search {
+
+    public void BookSearch(String BookName);
+
+    public void BookSearch(String BookName, ArrayList array);
+}
